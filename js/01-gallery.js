bff5ddb3c66c29fe812ec08a26590c9de8e6b505
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-10-24 00:58:39 +0300

    Решает задание 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,83 +1,105 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+// 1
 
 const galleryContainer = document.querySelector('.gallery');
 
-const imageMarkup = createImagesMarkup(galleryItems);
-imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+const createitem = galleryItems
+  .map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`,
+  )
+    .join('');
 
-console.log(createImagesMarkup(galleryItems));
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+// 2
+
+galleryContainer.addEventListener('click', openImage);
+
+function openImage(event) {
+  event.preventDefault();
+
+  // Фильтр цели
+  const targetImg = event.target.nodeName === 'IMG';
+  if (!targetImg) {
+    return;
+  }
 
 
-// const imgMarkUp = renderGallery(galleryItems);
-// galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
-// function renderGallery(items) {
-//   return items
-//     .map(item => {
-//       return `<div class="gallery__item">
-//             <a class="gallery__link" href=" ">
-//                 <img
-//                     class="gallery__image"
-//                     src=" "
-//                     data-source=" "
-//                     alt=" "/>
-//              </a>
-//         </div>`;
-//     })
-//     .join(' ');
+  const instance = basicLightbox.create(
+    `<div class="modal">
+      <img src = ${event.target.dataset.source}
+        alt=${event.target.alt}
+        width = '1140'
+        height = '800'
+      />
+    </div> `,
+
+    {
+      onShow: instance => {
+        instance.element().querySelector('img').onclick = instance.close;
+        
+        window.addEventListener('keydown', eventHandler);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', eventHandler);
+      },
+    },
+  );
+        function eventHandler(event) {
+          if (event.key === 'Escape') {
+            instance.close();
+            return;
+          }
+        }
+
+  instance.show();
+}
+
+
+// const instance = basicLightbox.create(`
+//     <div class="modal">
+//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
+//         <input placeholder="Type something">
+//         <a>Close</a>
+//     </div>
+// `, {
+//     onShow: (instance) => {
+//         instance.element().querySelector('a').onclick = instance.close
+//     }
+// })
+
+// instance.show()
+
+
+
+// ************************************************************************
+// const preview = galleryItems.map(galleryItem => galleryItem.preview);
+// const original = galleryItems.map(galleryItem => galleryItem.original);
+// const description = galleryItems.map(galleryItem => galleryItem.description);
+// 
+// let imageMarkup = "";
+// for (let i = 0; i < 9; i++) {
+    
+//     imageMarkup += `<div class="gallery__item">
+//     <a class="gallery__link"
+//         href="${original[i]}">
+//         <img
+//         class="gallery__image"
+//         src="${preview[i]}"
+//         data-source="${original[i]}"
+//         alt="${description[i]}"
+//         />
+//     </a>
+//     </div> `     
 // }
-
-
-// const imageContainer = document.querySelector('.gallery');
-// const imageMarkup = createImagesMarkup(galleryItems);
-// imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
-
-// //console.log(createImagesMarkup(galleryItems));
-
-// function createImagesMarkup(images) {
-//   return images
-//     .map(({ preview, original, description }) => {
-//       return `<li class="gallery__item">
-// <a
-//   class="gallery__link"
-//   href='${original}'
-// >
-//   <img
-//     class="gallery__image"
-//     src='${preview}'
-//     data-source='${original}'
-//     alt='${description}'
-//   />
-// </a>
-// </li>`;
-//     })
-//     .join('');
-// }
-
-
-
-
-
-
-
-// const imgMarkUp = renderGallery(galleryItems);
-// galleryContainer.insertAdjacentHTML('beforeend', imgMarkUp);
-// function renderGallery(items) {
-//   return items
-//     .map(item => {
-//       return `<div class="gallery__item">
-//             <a class="gallery__link" href=" ">
-//                 <img
-//                     class="gallery__image"
-//                     src=" "
-//                     data-source=" "
-//                     alt=" "
-//                 />
-//              </a>
-//         </div>`;
-//     })
-//     .join(' ');
-// }
-
-console.log(galleryItems);
+// document.querySelector('.gallery').innerHTML = imageMarkup;
+// ************************************************************************
