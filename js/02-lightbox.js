22ace8b1aa62c513eed40774f2288d5a4f42661a
Daemon-L <lit.dmitriy.ua@gmail.com>
2021-10-24 12:58:29 +0300

    Выполняет задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// 1
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const createitem = galleryItems
+  .map(({ preview, original, description }) =>
+      
+    `<li class="item">
+        <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`,
+  )
+    .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', createitem);
+
+
+// 2
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: `250`,
+    captionPosition: 'bottom',
+});
+
+
